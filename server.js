136b9c73a10152b8c084bdb20e4ded72720dd9a8
John Clayton <john@codemonkeylabs.com>
2013-05-19 00:01:36 -0600

    Clustering

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-var port = process.env.PORT || 3000,
-	http = require('http');
+var http = require('http'),
+	cluster = require('cluster'),
+	numCPUs = require('os').cpus().length;
 
-http.createServer(function (req, res) {
-	var body = "bar";
-	if (req.url.indexOf("loaderio") != -1) {
-		body = req.url.substr(1, req.url.length - 5);
-	} else if (req.url === "/env") {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify(process.env));
-	}
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end(body)
-}).listen(process.env.PORT || 3000);
+if (cluster.isMaster) {
+	for (var i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	};
+} else {
+	http.createServer(function (req, res) {
+		var body = "bar";
+		if (req.url.indexOf("loaderio") != -1) {
+			body = req.url.substr(1, req.url.length - 5);
+		} else if (req.url === "/env") {
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.end(JSON.stringify(process.env));
+		}
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end(body)
+	}).listen(process.env.PORT || 3000);
+}
