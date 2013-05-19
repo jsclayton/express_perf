ddb5bea6b65fe547b055843d4c03104cd020c6d6
John Clayton <john@codemonkeylabs.com>
2013-05-18 21:02:14 -0600

    Get env vars for debugging

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var port = process.env.PORT || 3000,
 	http = require('http');
 
 http.createServer(function (req, res) {
+	var body = "bar";
+	if (req.url.indexOf("loaderio") != -1) {
+		body = req.url.substr(1, req.url.length - 5);
+	} else if (req.url === "/env") {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(process.env));
+	}
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end(req.url.substr(1, req.url.length - 5))
+	res.end(body)
 }).listen(process.env.PORT || 3000);
